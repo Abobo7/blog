24705d39c8096773aa3ed475baa8b9a98fc7a06e
Abobo7 <2557826346@qq.com>
2025-01-27 23:10:54 +0800

    2025-01-27

---
+++ source/comments/gitalk.js
@@ -0,0 +1,33 @@
+(() => {
+    const loadComments = async () => {
+      if (typeof Gitalk === "undefined") {
+        setTimeout(loadComments, 100);
+      } else {
+        const container = document.getElementById('gitalk-container');
+        if (!container) {
+          return;
+        }
+        const path = container.getAttribute("data-path");
+        const gitalk = new Gitalk(Object.assign({
+            id: path, // 直接使用路径作为 id
+            // id: container.getAttribute("data-path-hash"), // 使用 hash 作为 ID
+            path: path,
+        }, {
+          clientID: 'Ov23lix7SAtAKyG3enHk',
+          clientSecret: 'e1089d7db37d3112af319912af43aa74136c925f',
+          repo: "blog_comments",
+          owner: "Abobo7",
+          admin: ["Abobo7"],
+          distractionFreeMode: false,
+        }));
+  
+        gitalk.render('gitalk-container');
+      }
+    };
+  
+    window.loadComments = loadComments;
+    window.addEventListener('pjax:success', () => {
+      window.loadComments = loadComments;
+    });
+  })();
+  
